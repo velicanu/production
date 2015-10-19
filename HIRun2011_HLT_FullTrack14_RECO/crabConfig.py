c1349119f502f7fe8b5852655016f21d881567c5
velicanu <velicanu@mit.edu>
2015-10-19 14:48:09 -0400

    Update crabConfig.py

--- HIRun2011_HLT_FullTrack14_RECO/crabConfig.py
+++ HIRun2011_HLT_FullTrack14_RECO/crabConfig.py
@@ -3,10 +3,10 @@ config = config()
 
 #### JobType ####
 config.JobType.pluginName = "Analysis"
-config.JobType.psetName = "runForestAOD_PbPb_DATA_75X.py"
+config.JobType.psetName = "step3_RAW2DIGI_L1Reco_RECO.py"
 
 #### Data ####
-config.Data.inputDataset = "/HIHighPt/velicanu-HIHighPt_HIRun2011_HLT_HISinglePhoton20_HISinglePhoton30_RECO_753p1-fd44351629dd155a25de2b4c109c824c/USER"
+config.Data.inputDataset = "/HIHighPt/dgulhan-HIHighPt_HIRun2011_HLT_HIFullTrack14-9a51de932e7ef7c4280ba90e9fc423d7/USER"
 # config.Data.useParent = True
 config.Data.inputDBS = "phys03"
 config.Data.splitting = "FileBased"
@@ -14,7 +14,7 @@ config.Data.unitsPerJob = 1
 config.Data.totalUnits = -1
 
 config.Data.outLFNDirBase = "/store/user/velicanu/"
-config.Data.publishDataName = "HIHighPt_HIRun2011_HLT_HISinglePhoton20_HISinglePhoton30_FOREST_753p1_v0"
+config.Data.publishDataName = "HIHighPt_HIRun2011_HLT_HIFullTrack14_RECO_753p1"
 
 #### Site ####
 config.Site.storageSite = "T2_US_MIT"

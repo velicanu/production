7672637db25dfa12369e2bee3611750703a04cdd
Dragos Velicanu <velicanu@mit.edu>
2015-11-02 10:55:49 +0100

    crabConfig edited

--- QCD_Pt_X_X_5020GeV_TuneCUETP8M1_cfi_GEN_SIM_PU_GENSIM/crabConfig.py
+++ QCD_Pt_X_X_5020GeV_TuneCUETP8M1_cfi_GEN_SIM_PU_GENSIM/crabConfig.py
@@ -1,25 +1,21 @@
-from CRABClient.UserUtilities import config
-config = config()
-
-#### JobType ####
-config.JobType.pluginName = "Analysis"
-config.JobType.psetName = "step3_RAW2DIGI_L1Reco_RECO.py"
-
-#### Data ####
-config.Data.inputDataset = "/HIHighPt/dgulhan-HIHighPt_HIRun2011_HLT_HIFullTrack14-9a51de932e7ef7c4280ba90e9fc423d7/USER"
-# config.Data.useParent = True
-config.Data.inputDBS = "phys03"
-config.Data.splitting = "FileBased"
-config.Data.unitsPerJob = 1
-config.Data.totalUnits = -1
-
-config.Data.outLFNDirBase = "/store/user/velicanu/"
-config.Data.publishDataName = "HIHighPt_HIRun2011_HLT_HIFullTrack14_RECO_753p1"
-
-#### Site ####
-config.Site.storageSite = "T2_US_MIT"
-#config.Site.whitelist = ["T2_US_MIT","T2_US_CERN","T2_US_Vanderbilt"]
-config.Site.whitelist = ["T2_US_MIT"]
-#config.Site.blacklist = ["T0","T1"].
-config.Debug.extraJDL = ['+CMS_ALLOW_OVERFLOW=False']
-
+from WMCore.Configuration import Configuration
+config = Configuration()
+config.section_('General')
+config.General.transferOutputs = True
+config.section_('JobType')
+config.JobType.psetName = 'QCD_TuneCUETP8M1_cfi_GEN_SIM.py'
+config.JobType.pluginName = 'PrivateMC'
+config.JobType.inputFiles = ['rssLimit']
+config.JobType.outputFiles = ['step1.root']
+config.section_('Data')
+config.Data.primaryDataset = 'PYTHIA_QCD_TuneCUETP8M1_cfi_GEN_SIM_5020GeV'
+config.Data.splitting = 'EventBased'
+config.Data.publication = True
+config.Data.unitsPerJob = 100
+NJOBS = 3000
+config.Data.totalUnits = config.Data.unitsPerJob * NJOBS
+config.Data.publishDataName = 'QCD_TuneCUETP8M1_cfi_GEN_SIM'
+config.section_('User')
+config.section_('Site')
+config.Site.whitelist = ['T2_US_MIT']
+config.Site.storageSite = 'T2_US_MIT'

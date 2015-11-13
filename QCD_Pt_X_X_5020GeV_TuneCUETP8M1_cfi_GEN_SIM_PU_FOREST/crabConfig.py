70f33ca0cfd72fad695bbf124923e091dc0c5431
Dragos Velicanu <velicanu@mit.edu>
2015-11-13 12:56:25 +0100

    added dogas forest production dir

---
+++ QCD_Pt_X_X_5020GeV_TuneCUETP8M1_cfi_GEN_SIM_PU_FOREST/crabConfig.py
@@ -0,0 +1,23 @@
+from WMCore.Configuration import Configuration
+config = Configuration()
+config.section_('General')
+config.General.transferOutputs = True
+config.section_('JobType')
+config.JobType.psetName = '_PSETFLAG_'
+config.JobType.pluginName = 'Analysis'
+config.JobType.inputFiles = ['rssLimit','HI_PythiaCUETP8M1_5020GeV_753p1_v4.db']
+config.JobType.outputFiles = ['HiForest.root']
+config.section_('Data')
+config.Data.inputDataset = '_DATASETFLAG_'
+config.Data.inputDBS = 'phys03'
+config.Data.splitting = 'EventAwareLumiBased'
+config.Data.publication = True
+config.Data.unitsPerJob = 200
+# config.Data.publishDataName = 'HiForest_PYTHIA_QCD_30_TuneCUETP8M1_cfi_HI_5020GeV_lite_JEC'
+config.Data.outputDatasetTag = 'HiForest_HIReco_PYTHIA_QCD_PTMINFLAG__TuneCUETP8M1_cfi_5020GeV_tag_HiForestPPSignalJECv3_dbJECv4'
+config.section_('User')
+config.section_('Site')
+#config.Site.whitelist = ['T2_CH_CERN']
+config.Site.storageSite = 'T2_US_MIT'
+config.section_("Debug")
+config.Debug.extraJDL = ['+CMS_ALLOW_OVERFLOW=False']

396f890f7c86154f50b7d321c233fb50371d1bfd
Dragos Velicanu <velicanu@mit.edu>
2015-11-14 23:19:48 +0100

    crab submission ready

--- MinBias_TuneCUETP8M1_5p02TeV-pythia8_RECO/crabConfig.py
+++ MinBias_TuneCUETP8M1_5p02TeV-pythia8_RECO/crabConfig.py
@@ -3,18 +3,18 @@ config = Configuration()
 config.section_('General')
 config.General.transferOutputs = True
 config.section_('JobType')
-config.JobType.psetName = '_PSETFLAG_'
+config.JobType.psetName = 'step3_RAW2DIGI_L1Reco_RECO.py'
 config.JobType.pluginName = 'Analysis'
-config.JobType.inputFiles = ['rssLimit']
-config.JobType.outputFiles = ['step2.root']
+# config.JobType.inputFiles = ['rssLimit']
+# config.JobType.outputFiles = ['step2.root']
 config.section_('Data')
-config.Data.inputDataset = '_DATASETFLAG_'
+config.Data.inputDataset = '/MinBias_TuneCUETP8M1_5p02TeV-pythia8/twang-MinBias_TuneCUETP8M1_5p02TeV_pythia8_pp502Fall15_MCRUN2_71_V1_v1_step2_CMSSW_7_4_15_20151105-792724e95bc6b3667ad91f57fcdd4eac/USER'
 config.Data.inputDBS = 'phys03'
 config.Data.splitting = "FileBased"
 config.Data.unitsPerJob = 1
 config.Data.totalUnits = -1
 config.Data.publication = True
-config.Data.publishDataName = 'Pythia8_Dijet_PTMINFLAG__pp_TuneCUETP8M1_Hydjet_MinBias_5020GeV_DIGIRAW_PrivMC'
+config.Data.publishDataName = 'MinBias_TuneCUETP8M1_5p02TeV-pythia8_RECO'
 config.section_('User')
 config.section_('Site')
 config.Site.whitelist = ['T2_US_MIT']

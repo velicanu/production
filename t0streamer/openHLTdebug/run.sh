b63eefa834610b98f741c57149e0718a610a181c
Dragos Velicanu <velicanu@mit.edu>
2015-11-16 16:57:18 +0100

    update

--- t0streamer/openHLTdebug/run.sh
+++ t0streamer/openHLTdebug/run.sh
@@ -1,20 +1,23 @@
-if [ $# -ne 0 ]
+if [ $# -ne 1 ]
 then
-  echo "Usage: ./run.sh"
+  # echo "Usage: ./run.sh </path/to/t0streamer/files>"
+  # echo "Example: ./run.sh /store/t0streamer/Data/Express/000/261/396"
+  echo "Usage: ./run.sh <inputlist>"
+  echo "Example: ./run.sh 261445.txt"
   exit 1
 fi
 
-
+nfiles=`wc -l $1 | awk '{print $1}'`
+ntimes=$((${nfiles}/23+1))
 count=0
-for i in `seq 1 8`
+for i in `seq 1 ${ntimes}`
 do
   num=$((count*23+23))
-  for i in `head -n${num} 261445.txt | tail -n23`
+  for i in `head -n${num} $1 | tail -n23`
   do
     sed "s@_inputfileflag_@${i}@g ; s@_outputfileflag_@OpenHLT_Lumi${count}.root@g " openHLTfromStreamer.py > run${count}.py
     cmsRun run${count}.py &> run${count}.py.log &
     count=$((count+1))
-    exit
   done
 
   while [ `ps | wc -l` -gt 10 ]

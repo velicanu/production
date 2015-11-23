bf111217fccba04480ed5cb6ecb22a55434360c9
Dragos Velicanu <velicanu@mit.edu>
2015-11-23 01:35:47 +0100

    added

---
+++ HIRun2015ForestingSetup_v0/hireco_RAW2DIGI_L1Reco_RECO.py
@@ -0,0 +1,105 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.19 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
+# with command line options: hireco --conditions 75X_dataRun2_ExpressHI_v2 -s RAW2DIGI,L1Reco,RECO --runUnscheduled --process RECO --data --eventcontent FEVT --scenario HeavyIons --datatier RECO --customise Configuration/DataProcessing/RecoTLR.customiseRun2ExpressHI -n -1 --no_exec --filein file:blah --repacked
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('RECO')
+
+process.options = cms.untracked.PSet(
+    # wantSummary = cms.untracked.bool(True)
+    #SkipEvent = cms.untracked.vstring('ProductNotFound')
+)
+
+#parse command line arguments
+from FWCore.ParameterSet.VarParsing import VarParsing
+options = VarParsing('analysis')
+options.register ('isPP',
+                  False,
+                  VarParsing.multiplicity.singleton,
+                  VarParsing.varType.bool,
+                  "Flag if this is a pp simulation")
+options.parseArguments()
+
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContentHeavyIons_cff')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff')
+process.load('Configuration.StandardSequences.RawToDigi_Data_cff')
+process.load('Configuration.StandardSequences.L1Reco_cff')
+process.load('Configuration.StandardSequences.ReconstructionHeavyIons_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(options.maxEvents)
+)
+
+# Input source
+process.source = cms.Source("NewEventStreamFileReader",
+                            fileNames = cms.untracked.vstring(options.inputFiles[0])
+)
+
+process.options = cms.untracked.PSet(
+    allowUnscheduled = cms.untracked.bool(True)
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    annotation = cms.untracked.string('hireco nevts:-1'),
+    name = cms.untracked.string('Applications'),
+    version = cms.untracked.string('$Revision: 1.19 $')
+)
+
+# Output definition
+
+process.FEVToutput = cms.OutputModule("PoolOutputModule",
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('RECO'),
+        filterName = cms.untracked.string('')
+    ),
+    eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
+    fileName = cms.untracked.string(options.outputFile),
+    outputCommands = process.FEVTEventContent.outputCommands,
+    splitLevel = cms.untracked.int32(0)
+)
+
+# Additional output definition
+
+# Other statements
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, '75X_dataRun2_ExpressHI_v2', '')
+
+# Path and EndPath definitions
+process.raw2digi_step = cms.Path(process.RawToDigi)
+process.L1Reco_step = cms.Path(process.L1Reco)
+process.reconstruction_step = cms.Path(process.reconstructionHeavyIons)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.FEVToutput_step = cms.EndPath(process.FEVToutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.raw2digi_step,process.L1Reco_step,process.reconstruction_step,process.endjob_step,process.FEVToutput_step)
+
+from Configuration.Applications.ConfigBuilder import MassReplaceInputTag
+MassReplaceInputTag(process)
+
+# customisation of the process.
+
+# Automatic addition of the customisation function from Configuration.DataProcessing.RecoTLR
+from Configuration.DataProcessing.RecoTLR import customiseRun2ExpressHI 
+
+#call to customisation function customiseRun2ExpressHI imported from Configuration.DataProcessing.RecoTLR
+process = customiseRun2ExpressHI(process)
+
+# End of customisation functions
+#do not add changes to your config after this point (unless you know what you are doing)
+from FWCore.ParameterSet.Utilities import convertToUnscheduled
+process=convertToUnscheduled(process)
+from FWCore.ParameterSet.Utilities import cleanUnscheduled
+process=cleanUnscheduled(process)
+

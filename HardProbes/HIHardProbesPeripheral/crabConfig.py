ee2a560aa1f9466e595261c2b3a7afc9e9321aeb
Dragos Velicanu <velicanu@mit.edu>
2016-01-23 01:23:43 +0100

    added stuff

---
+++ HardProbes/HIHardProbesPeripheral/crabConfig.py
@@ -0,0 +1,24 @@
+from WMCore.Configuration import Configuration
+config = Configuration()
+config.section_('General')
+config.General.transferOutputs = True
+config.General.requestName = 'HIRun2015-ZMM-PromptReco-v1-FOREST-v11'
+config.section_('JobType')
+config.JobType.psetName = 'runForestAOD_PbPb_DATA_75X.py'
+# config.JobType.maxMemoryMB = 3000 #default 2000
+config.JobType.pluginName = 'Analysis'
+# config.JobType.outputFiles = ['HiForestAOD.root']
+config.section_('Data')
+config.Data.inputDataset = '/HIEWQExo/HIRun2015-ZMM-PromptReco-v1/RAW-RECO'
+# config.Data.inputDBS = 'global'
+config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/HI/DCSOnly/json_DCSONLY.txt'
+config.Data.splitting = "FileBased"
+config.Data.unitsPerJob = 1
+config.Data.totalUnits = -1
+config.Data.publication = False
+config.Data.outputDatasetTag = 'HIRun2015-ZMM-PromptReco-v1-FOREST-v11'
+config.section_('User')
+config.section_('Site')
+config.Site.whitelist = ['T2_US_MIT']
+config.Site.blacklist = ['T2_US_Nebraska']
+config.Site.storageSite = 'T2_US_MIT'
---
+++ Minbias_MC/PbPb_FOREST/crabConfig.py
@@ -0,0 +1,23 @@
+from WMCore.Configuration import Configuration
+config = Configuration()
+config.section_('General')
+config.General.transferOutputs = True
+config.General.requestName = 'Hydjet_Quenched_MinBias_5020GeV_758p2_FOREST_v20'
+config.section_('JobType')
+config.JobType.psetName = 'runForestAOD_PbPb_MIX_75X.py'
+# config.JobType.maxMemoryMB = 3000 #default 2000
+config.JobType.pluginName = 'Analysis'
+# config.JobType.outputFiles = ['HiForestAOD.root']
+config.section_('Data')
+config.Data.inputDataset = '/Hydjet_Quenched_MinBias_5020GeV_750/velicanu-Hydjet_Quenched_MinBias_5020GeV_758p2_RECODEBUG_v0-374be93f4012329d5cdc100aeee72e76/USER'
+config.Data.inputDBS = 'phys03'
+config.Data.splitting = "EventAwareLumiBased"
+config.Data.unitsPerJob = 1000
+config.Data.totalUnits = 100000
+config.Data.publication = False
+config.Data.outputDatasetTag = 'Hydjet_Quenched_MinBias_5020GeV_758p2_FOREST_v20'
+config.section_('User')
+config.section_('Site')
+config.Site.whitelist = ['T2_US_MIT']
+config.Site.blacklist = ['T2_US_Nebraska']
+config.Site.storageSite = 'T2_US_MIT'

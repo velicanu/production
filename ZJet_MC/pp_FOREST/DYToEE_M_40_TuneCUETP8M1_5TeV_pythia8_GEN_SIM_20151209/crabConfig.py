efe4e14defc8343c8a1a0ebd9fc83ac85b012641
Dragos Velicanu <velicanu@mit.edu>
2016-02-12 16:47:34 +0100

    v24 production

---
+++ ZJet_MC/pp_FOREST/DYToEE_M_40_TuneCUETP8M1_5TeV_pythia8_GEN_SIM_20151209/crabConfig.py
@@ -0,0 +1,23 @@
+from WMCore.Configuration import Configuration
+config = Configuration()
+config.section_('General')
+config.General.transferOutputs = True
+config.General.requestName = 'DYToEE_M_40_TuneCUETP8M1_5TeV_pythia8_GEN_SIM_20151209_v20'
+config.section_('JobType')
+config.JobType.psetName = 'runForestAOD_pp_MC_75X.py'
+# config.JobType.maxMemoryMB = 3000 #default 2000
+config.JobType.pluginName = 'Analysis'
+# config.JobType.outputFiles = ['HiForestAOD.root']
+config.section_('Data')
+config.Data.inputDataset = '/DYToEE_M_40_TuneCUETP8M1_5TeV_pythia8_GEN_SIM_20151209/twang-DYToEE_M_40_TuneCUETP8M1_5TeV_pythia8_step3_20160114-9ef792173553b51399f38cbb33b2e565/USER'
+config.Data.inputDBS = 'phys03'
+config.Data.splitting = "FileBased"
+config.Data.unitsPerJob = 2
+config.Data.totalUnits = -1
+config.Data.publication = False
+config.Data.outputDatasetTag = 'DYToEE_M_40_TuneCUETP8M1_5TeV_pythia8_GEN_SIM_20151209_v20'
+config.section_('User')
+config.section_('Site')
+config.Site.whitelist = ['T2_US_MIT']
+config.Site.blacklist = ['T2_US_Nebraska']
+config.Site.storageSite = 'T2_US_MIT'
---
+++ ZJet_MC/pp_FOREST/pp_Pythia8_Ze10e10_m60120_pthat0_TuneCUETP8M1_5020GeV_GEN_SIM_20160110/crabConfig.py
@@ -0,0 +1,23 @@
+from WMCore.Configuration import Configuration
+config = Configuration()
+config.section_('General')
+config.General.transferOutputs = True
+config.General.requestName = 'pp_Pythia8_Ze10e10_m60120_pthat0_TuneCUETP8M1_5020GeV_GEN_SIM_20160110_FOREST_v20'
+config.section_('JobType')
+config.JobType.psetName = 'runForestAOD_pp_MC_75X.py'
+# config.JobType.maxMemoryMB = 3000 #default 2000
+config.JobType.pluginName = 'Analysis'
+# config.JobType.outputFiles = ['HiForestAOD.root']
+config.section_('Data')
+config.Data.inputDataset = '/pp_Pythia8_Ze10e10_m60120_pthat0_TuneCUETP8M1_5020GeV_GEN_SIM_20160110/twang-pp_Pythia8_Ze10e10_m60120_pthat0_TuneCUETP8M1_5020GeV_step3_20160110-9ef792173553b51399f38cbb33b2e565/USER'
+config.Data.inputDBS = 'phys03'
+config.Data.splitting = "FileBased"
+config.Data.unitsPerJob = 5
+config.Data.totalUnits = -1
+config.Data.publication = False
+config.Data.outputDatasetTag = 'pp_Pythia8_Ze10e10_m60120_pthat0_TuneCUETP8M1_5020GeV_GEN_SIM_20160110_FOREST_v20'
+config.section_('User')
+config.section_('Site')
+config.Site.whitelist = ['T2_US_MIT']
+config.Site.blacklist = ['T2_US_Nebraska']
+config.Site.storageSite = 'T2_US_MIT'
